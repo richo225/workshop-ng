123775e774df706d43f3a4849e0dfe9cd14ece82
Richard Bates <richo225@gmail.com>
2016-10-09 12:38:46 +0100

    Add splash screen with kitten animations

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,40 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
+import { Animation } from "ui/animation";
 
 @Component({
     selector: "my-app",
-    templateUrl: "app.component.html",
+    templateUrl: "app.component.html"
 })
 export class AppComponent {
-    public counter: number = 16;
+  public kittens: Array<any>;
+  public url: string;
+  public counter:number = 200;
 
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
+  constructor() {
+    this.kittens = [];
+    this.url = 'https://placekitten.com/200/';
+
+    for (var i = 0; i < 10; i++) {
+      this.counter++;
+      this.kittens.push(this.url+this.counter);
     }
-    
-    public onTap() {
-        this.counter--;
-    }
+  }
+
+  animate(card) {
+    card.animate({ opacity: 0 })
+      .then(function () { return card.animate({ opacity: 1 }); })
+      .then(function () { return card.animate({ translate: { x: 100, y: 100 } }); })
+      .then(function () { return card.animate({ translate: { x: 0, y: 0 } }); })
+      .then(function () { return card.animate({ scale: { x: 3, y: 3 } }); })
+      .then(function () { return card.animate({ scale: { x: 1, y: 1 } }); })
+      .then(function () { return card.animate({ rotate: 180 }); })
+      .then(function () { return card.animate({ rotate: 0 }); })
+      .then(function () {
+      console.log("Animation finished");
+    })
+    .catch(function (e) {
+      console.log(e.message);
+    });
+}
+
 }
